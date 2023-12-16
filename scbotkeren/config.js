2854d192006a34681e41fd317a228913b701940f
YoshCasaster <132802733+YoshCasaster@users.noreply.github.com>
2023-12-16 13:10:30 +0700

    UP

---
+++ scbotkeren/config.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://api.lolhuman.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://api.lolhuman.xyz': 'YoshCasaster',
+}
+
+// Other
+global.owner = ['6281935593316']
+global.premium = ['6281935593316']
+global.packname = '𝗞𝗮𝘆𝘇𝗲𝗻𝗢𝗳𝗳𝗶𝗰𝗶𝗮𝗹'
+global.author = 'WhatsApp Bot'
+global.sessionName = 'hisoka'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
